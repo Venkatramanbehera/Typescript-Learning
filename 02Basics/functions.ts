80f73b9d3d6cb935f6364f765164a66ba3aadb18
Venkat <venkatramanbehera1@gmail.com>
2022-09-14 00:18:24 +0530

    function return type

--- 02Basics/functions.ts
+++ 02Basics/functions.ts
@@ -1,5 +1,6 @@
-function addTwo(num: number) {
+function addTwo(num: number): number {
     return num + 2;
+    // return "hello";
 }
 
 function getUpper(val: string) {
@@ -17,6 +18,23 @@ let logInUser = (name: string, email: string, isPaid: boolean = false) => {
 addTwo(5);
 getUpper("a");
 signUpUser("chiku","chiku@gmail.com",false)
-logInUser("chiku","chiku@gmail.com")
+logInUser("chiku", "chiku@gmail.com")
+
+
+const getHello = (s: string) : string => {
+    return "";
+}
+
+const heros = ["thor", "spiderman", "ironman"];
+
+heros.map((hero): string => `hero is ${hero}`)
+
+function consoleError(errmsg): void {
+    console.log(errmsg);
+}
+
+function handleError(errmsg): never {
+    throw new Error(errmsg);
+}
 
 export {}

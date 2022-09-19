2cd9cf6f56652465e13ff629efd6e86551e1051b
Venkat <venkatramanbehera1@gmail.com>
2022-09-19 10:09:42 +0530

    readonly and optional types

--- 02Basics/types.js
+++ 02Basics/types.js
@@ -1,2 +1,12 @@
 "use strict";
 exports.__esModule = true;
+function createUser(user) {
+    return user;
+}
+createUser({ email: '', name: '', isActive: false });
+var myUser = {
+    _id: '16hhjd',
+    email: 'venkat@gmail.com',
+    name: 'Venkat'
+};
+myUser._id = 'dhdhdh';
--- 02Basics/types.ts
+++ 02Basics/types.ts
@@ -9,6 +9,34 @@ function createUser(user: User): User {
     return user;
 }
 
-createUser({email: '', name: '', isActive: false});
+createUser({ email: '', name: '', isActive: false });
+
+type UserDetails = {
+    readonly _id: string,
+    name: string,
+    email: string,
+    creditCardDetails?: number
+}
+
+let myUser: UserDetails = {
+    _id: '16hhjd',
+    email: 'venkat@gmail.com',
+    name: 'Venkat'
+}
+
+// myUser._id = 'dhdhdh'
+// error _id is readonly types
+
+type cardNumber = {
+    cardnumber : string
+}
+
+type cardDate = {
+    cardDate : string
+}
+
+type cardDetails = cardNumber & cardDate & {
+    cvv : number
+}
 
 export {}

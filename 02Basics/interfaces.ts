84185541520f7030612c4884f6b6c1da3539ae1d
Venkat <venkatramanbehera1@gmail.com>
2022-09-27 09:25:55 +0530

    interface vs type

--- 02Basics/interfaces.ts
+++ 02Basics/interfaces.ts
@@ -8,10 +8,24 @@ interface User {
     getCoupon(couponName: string, value: number): number
 }
 
-const venkat: User = {
+interface User {
+    githubToken : string
+}
+
+// Its called as reopening of the interface [IMP]
+
+interface Admin extends User {
+    role : 'admin' | 'ta' | 'student'
+}
+
+// we can extends the interface but not in case of type
+
+const venkat: Admin = {
     name: 'test@gmail.com',
     userId: 12,
     dbId: 66,
+    role: 'admin',
+    githubToken: 'fdfdfdf',
     endTrail: () => {
         return 'Trail End'
     },
